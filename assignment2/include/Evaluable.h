b83ca991c194227d55d7228cb5926937e048f676
Ahmad Yasseen <ary04@mail.aub.edu>
2017-03-10 19:39:36 +0200

    Created Listable and Evaluable interfaces

---
+++ assignment2/include/Evaluable.h
@@ -0,0 +1,12 @@
+
+//@Functional Interface 
+
+class Evaluable {
+
+	public:
+
+/** 
+Evaluates the evaluable object and stores its value internally (should be retrievable by another methods).
+*/
+	void evaluate() = 0;				
+};
---
+++ assignment2/include/Listable.h
@@ -0,0 +1,11 @@
+
+// @Functional Interface 
+
+class Listable {
+	
+	public:
+/**
+   Lists this object i.e. prints its identifying name 
+**/
+	void list();
+};
