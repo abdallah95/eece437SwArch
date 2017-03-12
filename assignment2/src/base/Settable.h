0cec9c912d6076777eba413ff8b09f5a7d84169d
Ahmad Yasseen <ary04@mail.aub.edu>
2017-03-12 13:06:14 +0200

    Some organizational changes

---
+++ assignment2/src/base/Settable.h
@@ -0,0 +1,14 @@
+
+/* @Functional Interface 
+
+	Classes implementing this interface allow direct setting of their values; they are mutable (vs. constant).
+
+*/
+
+
+class Settable {
+
+//TODO We have to deal with the parameter type of this method; a suggestion is to create a class space that abstracts the concept of Value
+	
+	void setState ();
+};
